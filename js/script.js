b71c29c39ae519cb258e2259ec68c7c6c1a0ee60
GabrielRezend3 <80123472+GabrielRezend3@users.noreply.github.com>
2024-12-29 16:40:55 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,35 +1,11 @@
+const menuHamburguer = document.querySelector('.menu-hamburguer');
+const navResponsive = document.querySelector('.nav-responsive');
 
-
-
-
-const menuHamburguer = document.querySelector('.menu-hamburguer')
-
-menuHamburguer.addEventListener('click', ()   => {
-
-
-console.log("OK");
-toggleMenu();
-
-
-});
-
-
-function toggleMenu () {
-
-    const nav = document.querySelector('.nav-responsive');
-    menuHamburguer.classList.toggle('change')
-
-    if(menuHamburguer.classList('change'))  {
-
-
-        nav.style.display = 'block'
-
-    } else {
-
-         nav.style.display = 'none'
-
-    }
-
-
-
+if (menuHamburguer && navResponsive) {
+    menuHamburguer.addEventListener('click', () => {
+        menuHamburguer.classList.toggle('change');
+        navResponsive.classList.toggle('visible');
+    });
+} else {
+    console.error('Menu hambúrguer ou navegação responsiva não encontrados!');
 }
